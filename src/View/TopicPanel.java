67f5db4c91b3fc4aae0ac7560d3e7e66b9a7b060
Bùi Mạnh Tuấn <51566591+bmtuan@users.noreply.github.com>
2020-06-02 16:12:13 +0700

    Update TopicPanel.java

--- src/View/TopicPanel.java
+++ src/View/TopicPanel.java
@@ -19,7 +19,8 @@ public class TopicPanel extends JPanel{
     private JSeparator[] separators;
 
     public TopicPanel(int level) {
-        
+        lm = new LevelModel(level);
+        int size = lm.getAllExerciseByLevel(level).size();
         panelLeft = new JPanel();
         panelRight = new JPanel();
         backLabel = new JLabel();
@@ -63,9 +64,8 @@ public class TopicPanel extends JPanel{
                 .addComponent(chooseTopicLabel, GroupLayout.PREFERRED_SIZE, 80, GroupLayout.PREFERRED_SIZE)
                 .addGap(0, 240, Short.MAX_VALUE))
         );
-
+        
         add(panelLeft);
-
         panelRight.setMaximumSize(new Dimension(600, 600));
         panelRight.setMinimumSize(new Dimension(600, 600));
         panelRight.setPreferredSize(new Dimension(600, 600));
@@ -74,17 +74,17 @@ public class TopicPanel extends JPanel{
         for(int i=0 ; i<3 ; i++){
             title[i] = new JLabel();
             title[i].setFont(new Font("Arial", 0, 18));
-            title[i].setText("Topic"+(i+1));
+            title[i].setText(lm.getAllExerciseByLevel(level).get(i).getTitle());
             title[i].setPreferredSize(new Dimension(600, 60));
 
             information[i] = new JLabel();
             information[i].setFont(new Font("Arial", 0, 14));
-            information[i].setText("Level:" + "     Time:" + "     HighScore:" );
+            information[i].setText("Level: "+lm.getLevel()+  "   Time: "+ lm.getAllExerciseByLevel(level).get(i).getTime() +"     HighScore: " + lm.getAllExerciseByLevel(level).get(i).getHighScore());
             information[i].setPreferredSize(new Dimension(600, 60));
 
             transcript[i] = new JLabel();
             transcript[i].setFont(new Font("Arial", 0, 14));
-            transcript[i].setText("Transcript:" );
+            transcript[i].setText(lm.getAllExerciseByLevel(level).get(i).getDescription());
             transcript[i].setPreferredSize(new Dimension(600, 60));
             }
 
